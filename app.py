7a5a63850af2e6dc9849197d42234ed9a3088499
vrajdesai78 <vrajdesai78@gmail.com>
2023-04-02 13:04:12 +0530

    Created test endpoint

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from utils.generateEDA import filterHTML
 from utils.trainModel import *
 import numpy as np
 import pickle
+import json
+import requests as req
 
 app = Flask(__name__)
 
@@ -35,12 +37,12 @@ def train():
         df = pd.read_csv(file)
         df = preProcess(df)
         print(df.columns)
-        # train, test = splitData(df, target, splitratio)        
-        # model = trainModel(train, problem[0], target)
-        # model.fit(train.drop(target, axis=1), train[target])
-        # print(model.score(test.drop(target, axis=1), test[target]))
-        # # save model into pickle file 
-        # pickle.dump(model, open('model.pkl', 'wb'))
+        train, test = splitData(df, target, splitratio)        
+        model = trainModel(train, problem[0], target)
+        model.fit(train.drop(target, axis=1), train[target])
+        print(model.score(test.drop(target, axis=1), test[target]))
+        # save model into pickle file 
+        pickle.dump(model, open('model.pkl', 'wb'))
         return render_template('project.html')
     return redirect('/project')
 
@@ -53,13 +55,58 @@ def deploy():
         # Get the data from the POST request.
         data = request.get_json(force=True)
 
-        # Convert data into 2d np array
-        data = np.array(data['params']).reshape(1, -1)
+        # Get the values from json file, preprocess it using encoding.json and convert it into numpy array
+        df = pd.DataFrame(data, index=[0])
+
+        # Read encoding.json file
+        with open('encoding.json', 'r') as f:
+            le_data = json.load(f)
+
+        
+        # Drop columns which were dropped during training
+        df = df.drop(le_data['dropped_columns'], axis=1)
+
+        # Perform label encoding according to le_data
+        for col in df.select_dtypes(include='object'):
+            df[col] = df[col].replace(le_data[col])
+
+        # Convert dataframe into 2d numpy array
+        data = df.to_numpy()
 
         # Make prediction using model loaded from disk as per the data.
         prediction = model.predict(data)
 
         return jsonify({'Output': str(prediction[0])})
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == 'POST':
+        params = {}
+        labels = []
+        values = []
+        for key in request.form.keys():
+            if key.startswith('param-label-'):
+                label = request.form[key]
+                labels.append(label)
+            elif key.startswith('param-value-'):
+                value = request.form[key]
+                values.append(value)
+        for i in range(len(labels)):
+            params[labels[i]] = values[i]
+
+        json_data = json.dumps(params)
+
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+        url = 'http://127.0.0.1:5000/deploy'
+
+        response = req.post(url, data=json_data, headers=headers)
+
+        print(response.text)
+
+        return render_template('deploy.html', params=params)
+
+    return render_template('deploy.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils/preProcess.py
+++ utils/preProcess.py
@@ -1,15 +1,34 @@
 import pandas as pd
+import json
 
 def preProcess(df): 
-    # Drop column if more than 50% of data of column is null else drop rows
-    df = df.dropna(axis=1, thresh=len(df)*0.5)
-    df = df.dropna(axis=0)
 
-    for col in df.select_dtypes(include=['object']):
-        if df[col].nunique() < (0.3 * df.shape[0]):
-            df = pd.concat([df, pd.get_dummies(df[col], prefix=col)], axis=1)
-            df.drop(columns=[col], inplace=True)
+    # Drop column if more than 50% of data of column is null else drop rows and store the dropped columns in a list
+    dropped_columns = []
+    for col in df.columns:
+        if df[col].isnull().sum() > len(df)*0.5:
+            df = df.drop(col, axis=1)
+            dropped_columns.append(col)
         else:
-            df.drop(columns=[col], inplace=True)
-    
+            df = df.dropna(axis=0)
+
+    le_data = {}
+    for col in df.select_dtypes(include='object'):
+        le_dict = {}
+        if(len(df[col].unique()) > 10):
+            df = df.drop(col, axis=1)
+            dropped_columns.append(col)
+            continue
+        for val in df[col].unique():
+            if val not in le_dict:
+                le_dict[val] = len(le_dict) + 1
+        le_data[col] = le_dict
+        
+        df[col] = df[col].replace(le_dict)
+
+    le_data['dropped_columns'] = dropped_columns
+
+    with open('encoding.json', 'w') as f:
+        json.dump(le_data, f)    
+
     return df
